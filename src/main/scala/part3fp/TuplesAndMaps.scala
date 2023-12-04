6229c5956c4362a8b78b322c9d9c4338fcfa6025
omirostr1 <ucabotr@ucl.ac.uk>
2023-12-04 14:30:00 +0000

    Monday afternoon practise and changes

--- src/main/scala/part3fp/TuplesAndMaps.scala
+++ src/main/scala/part3fp/TuplesAndMaps.scala
@@ -6,4 +6,15 @@ object TuplesAndMaps extends App{
   val aTuple = new Tuple2(2, "hello, Scala") // Tuple2[Int, String] = (Int, String)
   // can avoid keywords "new" and/or "Tuple2" when defining a new Tuple
 
+  println(aTuple._1) // 1st element which is 2
+  println(aTuple.copy(_2 = "goodbye Java"))
+  println(aTuple.swap) // ("hello, Scala", 2)
+
+  // Maps - associate keys -> values
+  val aMap: Map[String, Int] = Map()
+
+  val phonebook = Map(("Jim", 555), ("Daniel", 789), "Omiros" -> 357)
+  // a -> b is sugar for (a, b)
+  println(phonebook)
+
 }
