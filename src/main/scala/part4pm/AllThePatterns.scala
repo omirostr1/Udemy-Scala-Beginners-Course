07e93940d747a13ad4d3148a4a46f919eebcd312
omirostr1 <ucabotr@ucl.ac.uk>
2023-12-06 01:15:49 +0000

    Tuesday all day practise and changes

---
+++ src/main/scala/part4pm/AllThePatterns.scala
@@ -0,0 +1,40 @@
+package part4pm
+
+object AllThePatterns extends App{
+
+  // 1 - constants
+  val x: Any = "Scala"
+  val constants = x match {
+    case 1 => "a number"
+    case "Scala" => "THE Scala"
+    case true => "The Truth"
+    case AllThePatterns => "A singleton object"
+  }
+
+  // 2 - match anything
+  // 2.1 wildcard
+
+  val matchAnything = x match {
+    case _ =>
+  }
+
+  // 2.2 variable
+  val matchAVariable = x match {
+    case something => s"I've found $something"
+  }
+
+  // 3 - tuples
+  val aTuple = (1,2)
+  val matchATuple = aTuple match {
+    case (1, 1) => 1
+    case (something, 1) => s"I've found $something"
+  }
+
+  val nestedTuple = (1, (2, 3))
+  val matchANestedTuple = nestedTuple match {
+    case (_, (2, v)) =>
+  }
+
+  // PM can be nested
+
+}
